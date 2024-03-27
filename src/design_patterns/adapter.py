26dc23b3244a2fc168cda245d54a619c9d50bfdc
EgorSvetlichnyy <fkhor@inbox.ru>
2024-03-27 13:57:16 +0300

    add adapter realization

---
+++ src/design_patterns/adapter.py
@@ -0,0 +1,59 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ICurrency(metaclass=ABCMeta):
+    @abstractmethod
+    def get_count_money(self):
+        pass
+
+
+class RubCurrency(ICurrency):
+    def __init__(self, count_money):
+        self.__count_money = count_money
+
+    def get_count_money(self):
+        return self.__count_money
+
+
+class UsdCurrency(ICurrency):
+    def __init__(self, count_money):
+        self.__count_money = count_money
+
+    def get_count_money(self):
+        return self.__count_money
+
+
+class KrwCurrency(ICurrency):
+    def __init__(self, count_money):
+        self.__count_money = count_money
+
+    def get_count_money(self):
+        return self.__count_money
+
+
+class AdapterForRubCurrency(ICurrency):
+    def __init__(self, rub_currency: RubCurrency):
+        self.__rub_currency = rub_currency
+
+    def get_count_money(self):
+        return self.__rub_currency.get_count_money() * 0.010802
+
+
+class AdapterForKrwCurrency(ICurrency):
+    def __init__(self, krw_currency: KrwCurrency):
+        self.__krw_currency = krw_currency
+
+    def get_count_money(self):
+        return self.__krw_currency.get_count_money() * 0.000747
+
+
+if __name__ == '__main__':
+    money = 100
+
+    rub_currency = AdapterForRubCurrency(RubCurrency(money))
+    usd_currency = UsdCurrency(money)
+    krw_currency = AdapterForKrwCurrency(KrwCurrency(money))
+
+    print(f"100 рублей в долларах равно: {rub_currency.get_count_money()}")
+    print(f"100 долларов в долларах равно: {usd_currency.get_count_money()}")
+    print(f"100 вон в долларах равно: {krw_currency.get_count_money()}")
