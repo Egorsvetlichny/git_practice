0a777b9472085119a478f1f9ea727e2438c93d69
Egor Svetlichny <fkhor@inbox.ru>
2024-04-25 18:53:58 +0300

    add optimal solution for algorithms_task_12. From int seconds to 'HH:MM:SS'

--- src/interview_tasks/algorithms_task_12.py
+++ src/interview_tasks/algorithms_task_12.py
@@ -6,7 +6,7 @@ from typing import Union
 
 # Например: Input(7275), Output(02:01:15)
 
-
+# my solution
 def parentheses_accuracy(number: int) -> Union[str, bool]:
     if number < 0 or number > 359999:
         return False
@@ -18,13 +18,18 @@ def parentheses_accuracy(number: int) -> Union[str, bool]:
     return f'{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}'
 
 
+# optimal solution
+def optimal_parentheses_accuracy(number: int) -> Union[str, bool]:
+    return f'{(number // 3600):02d}:{(number % 3600 // 60):02d}:{(number % 60):02d}' if 0 <= number <= 359999 else False
+
+
 def main():
-    assert parentheses_accuracy(7275) == '02:01:15'
-    assert parentheses_accuracy(66666) == '18:31:06'
-    assert parentheses_accuracy(359999) == '99:59:59'
-    assert parentheses_accuracy(0) == '00:00:00'
-    assert parentheses_accuracy(360000) is False
-    assert parentheses_accuracy(-3213) is False
+    assert optimal_parentheses_accuracy(7275) == '02:01:15'
+    assert optimal_parentheses_accuracy(66666) == '18:31:06'
+    assert optimal_parentheses_accuracy(359999) == '99:59:59'
+    assert optimal_parentheses_accuracy(0) == '00:00:00'
+    assert optimal_parentheses_accuracy(360000) is False
+    assert optimal_parentheses_accuracy(-3213) is False
 
 
 if __name__ == '__main__':
