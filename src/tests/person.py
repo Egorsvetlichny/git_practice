3a2851358c76e096e34c8381fe0e3a7f84f958ee
Egor Svetlichny <fkhor@inbox.ru>
2024-04-30 13:14:48 +0300

    add exception unittests for person module

--- src/tests/person.py
+++ src/tests/person.py
@@ -9,6 +9,10 @@ class Person:
 
     @staticmethod
     def work(sleep_time=normal_sleep_time, got_eat=True):
+        if type(sleep_time) not in (int, float) or type(got_eat) != bool:
+            raise TypeError("Неверный тип данных одного из аргументов!")
+        if sleep_time <= 0 or sleep_time > 11 * 60 * 60:
+            raise ValueError("Время сна должно быть положительным числом меньше 11!")
         if not got_eat or sleep_time < Person.normal_sleep_time:
             return False
         return True
--- src/tests/unittests/test_person.py
+++ src/tests/unittests/test_person.py
@@ -1,6 +1,8 @@
 import unittest
 from tests.person import Person
 
+person = Person('Person')
+
 
 class TestPerson(unittest.TestCase):
     def test_work(self):
@@ -32,6 +34,39 @@ class TestPerson(unittest.TestCase):
         ]
 
         for test_case in test_cases:
-            person = Person('Person')
-
             self.assertIs(person.work(**test_case['arguments']), test_case['result'])
+
+    def test_work_exceptions(self):
+        test_cases = [
+            {
+                'arguments': {'sleep_time': -9.5 * 60 * 60, 'got_eat': True},
+                'exc': ValueError
+            },
+            {
+                'arguments': {'sleep_time': 0, 'got_eat': True},
+                'exc': ValueError
+            },
+            {
+                'arguments': {'sleep_time': 11.5 * 60 * 60},
+                'exc': ValueError
+            },
+            {
+                'arguments': {'sleep_time': '11 * 60 * 60'},
+                'exc': TypeError
+            },
+            {
+                'arguments': {'sleep_time': 11 * 60 * 60, 'got_eat': [4, 5, 1]},
+                'exc': TypeError
+            },
+            {
+                'arguments': {'sleep_time': True},
+                'exc': TypeError
+            },
+            {
+                'arguments': {'sleep_time': {}},
+                'exc': TypeError
+            },
+        ]
+
+        for test_case in test_cases:
+            self.assertRaises(test_case['exc'], person.work, **test_case['arguments'])
