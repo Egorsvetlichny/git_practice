e9e08f921997735eeaba1bbba941688e9b0e1f1f
Egor Svetlichny <fkhor@inbox.ru>
2024-05-03 10:01:26 +0300

    add recursion fibonacci function

---
+++ src/recursion_practice/recursion_fib.py
@@ -0,0 +1,25 @@
+# Write a function, which takes an input int "i" and must return fib number on "i" position.
+# Positions starts with 1, NOT 0!
+
+
+def rec_fib(i: int) -> int:
+    if i < 0:
+        return 'Incorrect input, i must be greater than 0!'
+    if i == 1:
+        return 1
+    if i == 2:
+        return 1
+    return rec_fib(i - 1) + rec_fib(i - 2)
+
+
+def main():
+    assert rec_fib(1) == 1
+    assert rec_fib(2) == 1
+    assert rec_fib(3) == 2
+    assert rec_fib(4) == 3
+    assert rec_fib(5) == 5
+    assert rec_fib(11) == 89
+
+
+if __name__ == '__main__':
+    main()
