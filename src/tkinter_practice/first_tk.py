4e2df780f843ca16e90110667d5f914dfbb33a87
EgorSvetlichnyy <fkhor@inbox.ru>
2024-06-04 19:04:31 +0300

    Position the window in the center of screen in first tkinter app

--- src/tkinter_practice/first_tk.py
+++ src/tkinter_practice/first_tk.py
@@ -4,6 +4,15 @@ import tkinter
 
 root = tkinter.Tk()
 root.title("Первое приложение на tkinter")
-root.geometry("900x600")
+
+# root.geometry("900x600")
+# Позиционирование окна в центре экрана
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = 900
+window_height = 600
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
+root.geometry(f'{window_width}x{window_height}+{x}+{y}')
 
 root.mainloop()
