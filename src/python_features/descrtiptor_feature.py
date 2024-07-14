19083aa95deba95a282ebfa5818b2f6731b7df3f
EgorSvetlichnyy <fkhor@inbox.ru>
2024-07-14 17:16:47 +0300

    add more advanced descriptor features

--- src/python_features/descrtiptor_feature.py
+++ src/python_features/descrtiptor_feature.py
@@ -111,3 +111,112 @@ u = User("Katya", 30)
 u.name = "Katyuha"  # Логируется изменение
 u.age = 31  # Логируется изменение
 
+
+# Example №5
+class Singleton:
+    def __init__(self, cls):
+        self.cls = cls
+        self.instance = None
+
+    def __get__(self, instance, owner):
+        if self.instance is None:
+            self.instance = self.cls()
+        return self.instance
+
+
+class Database:
+    def __init__(self):
+        print("Создание базы данных")
+
+
+# применение дескриптора Singleton
+class AppConfig:
+    db = Singleton(Database)
+
+
+# тестирование паттерна Singleton
+config1 = AppConfig()
+config2 = AppConfig()
+db1 = config1.db  # создание БД
+db2 = config2.db  # не создает новый экземпляр, использует существующий
+
+print(db1 is db2)  # выведет True, подтверждая, что db1 и db2 - один и тот же объект
+
+
+# Example №6
+class VehicleFactory:
+    def __init__(self, cls):
+        self.cls = cls
+
+    def __get__(self, instance, owner):
+        return self.cls()
+
+
+class Car:
+    @staticmethod
+    def drive():
+        print("Вождение автомобиля")
+
+
+class Bike:
+    @staticmethod
+    def ride():
+        print("Езда на велосипеде")
+
+
+# фабрика, создающая автомобили
+class AppConfigCar:
+    vehicle = VehicleFactory(Car)
+
+
+# фабрика, создающая велосипеды
+class AppConfigBike:
+    vehicle = VehicleFactory(Bike)
+
+
+# создание и использование автомобиля
+car_config = AppConfigCar()
+car = car_config.vehicle  # создает объект Car
+car.drive()
+
+# создание и использование велосипеда
+bike_config = AppConfigBike()
+bike = bike_config.vehicle  # создает объект Bike
+bike.ride()
+
+
+# Example №7
+# property - работает как дескриптор, используя методы __get__, __set__ и __delete__ для управления доступом к атрибуту
+class Celsius:
+    def __init__(self, temperature=0):
+        self.__temperature = temperature
+
+    @property
+    def temperature(self):
+        return self.__temperature
+
+    def to_fahrenheit(self):
+        return (self.__temperature * 1.8) + 32
+
+    def get_temperature(self):
+        print("Получение значения")
+        return self.__temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        if value < -273.15:
+            raise ValueError("Температура не может быть ниже -273.15 градусов Цельсия")
+        print("Установка значения")
+        self.__temperature = value
+
+
+c = Celsius(37)
+print(c.temperature)
+c.temperature = -300  # вызовет исключение
+
+# classmethod - реализован также как дескриптор. Когда метод декорирован как classmethod,
+# его вызов приводит к вызову метода __get__ дескриптора, который возвращает привязанный метод - функцию,
+# первым аргументом которой автоматически становится класс
+
+# staticmethod - также реализован как дескриптор.
+# При его использовании метод __get__ дескриптора просто возвращает функцию без привязки к экземпляру или классу
